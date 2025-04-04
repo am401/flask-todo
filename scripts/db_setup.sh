ce918968bea990f7659e31c9ba270661e2dc2792
am401 <54643953+am401@users.noreply.github.com>
2025-04-04 08:47:00 -0500

    added db setup script and tf

---
+++ scripts/db_setup.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+source /var/www/.env
+
+. venv/bin/activate
+
+yum install postgresql15.x86_64 postgresql15-server -y
+
+su postgres -c "pg_ctl init -D /var/lib/pgsql/data"
+
+systemctl start postgresql
+systemctl enable postgresql
+
+su postgres -c "psql -c \"CREATE USER flasktodo2 WITH PASSWORD '$(printf '%s' "$PSQL_PASSWORD")'\""
+su postgres -c 'createdb -e --owner=flasktodo todos'
+
+export FLASK_APP=wsgi
+
+systemctl restart flasktodo
+systemctl restart nginx
+
+flask db upgrade
